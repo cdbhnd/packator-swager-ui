8fc590bd7187937c3a34ddccc218b0c61b3584a6
aleksblack <alexandar.kovach@gmail.com>
2016-10-07 14:41:21 +0200

    Removed framework, only necessary files are included. Just put it under web server.

--- src/main/javascript/packator/packator.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    'use strict';
-
-    $('#api_select').change(function() {
-        var selectedApi = $('#api_select').val();
-        $('#input_baseUrl').val(selectedApi);
-        $('#explore').click();
-    });
-});
